658f63c0d4fe0843286bd3134aae977189f45d29
jackbigej <jbigej@nd.edu>
2021-11-13 14:46:14 -0500

    more processing :)

--- processData.py
+++ processData.py
@@ -3,7 +3,7 @@
 import sys
 import csv
 
-def import_data():
+def parse_schedule():
     with open('FallClassSchedule.csv', 'r') as f:
         csvreader = csv.reader(f)
 
@@ -23,11 +23,95 @@ def import_data():
                     pd[room][l].append(str(str(row[6]) + ' - ' + str(row[7])))
                     pd[room][l].sort()
 
-        print(pd)
+        translated = {}
+
+        for room in pd.keys():
+            if room not in translated:
+                translated[room] = {}
+            for day in pd[room].keys():
+                if day not in translated:
+                    translated[room][day] = [0 for i in range(288)]
+
+                for tp in pd[room][day]:
+                    tp = tp.split(' - ')
+
+                    start_end = {'start': {}, 'end':{}}
+                    #Start Time
+                    
+                    if tp[0].split(' ')[1] == 'PM':
+                        time = tp[0].split(' ')[0]
+                        time = time.split(':')
+                        if room == 'B011' and day == 'F':
+                            print(time)
+                        hour = int(time[0])
+                        minute = int(time[1])
+                        if hour != 12:
+                            hour = hour + 12
+                        start_end['start']['hour'] = hour
+                        start_end['start']['min'] = minute
+
+                        #print(hour, minute, tp[0].split(' ')[1])
+
+                    else:
+                        time = tp[0].split(' ')[0]
+                        time = time.split(':')
+                        if room == 'B011' and day == 'F':
+                            print(time)
+                        hour = int(time[0])
+                        minute = int(time[1])
+                        start_end['start']['hour'] = hour
+                        start_end['start']['min'] = minute
+                        #print(hour, minute, tp[0].split(' ')[1])
+
+
+                    # End Time
+
+                    if tp[1].split(' ')[1] == 'PM':
+                        time = tp[1].split(' ')[0]
+                        time = time.split(':')
+                        if room == 'B011' and day == 'F':
+                            print(time)
+                        hour = int(time[0])
+                        minute = int(time[1])
+                        if hour != 12:
+                            hour = hour + 12
+                        start_end['end']['hour'] = hour
+                        start_end['end']['min'] = minute
+                        #print(hour, minute, tp[0].split(' ')[1])
+
+                    else:
+                        time = tp[1].split(' ')[0]
+                        time = time.split(':')
+                        if room == 'B011' and day == 'F':
+                            print(time)
+                        hour = int(time[0])
+                        minute = int(time[1])
+                        start_end['end']['hour'] = hour
+                        start_end['end']['min'] = minute
+                        #print(hour, minute, tp[0].split(' ')[1])
+
+                    
+                    
+                    index_start = int((12*start_end['start']['hour']) + int((start_end['start']['min']/5)))
+                    index_end = int((12*start_end['end']['hour']) + int((start_end['end']['min']/5)))
+
+                    if room == 'B011' and day == 'F':
+                        print(start_end)
+                        print('Start:', index_start, 'End:', index_end)
+                    for i in range(index_start, index_end + 1):
+                        translated[room][day][i] = 1
+                    #print(len(translated[room][day]))
+
+        print(pd['B011']['F'])
+        print(translated)
+
+
+def parse_occupancy():
+    pass
 
 
 def main():
-    import_data()
+    parse_schedule()
 
 if __name__ == '__main__':
     main()
